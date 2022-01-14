13793e9bed0aea3d4935793e94445524a2b5e233
JoniMeichtry11 <joni.meichtry@gmail.com>
2022-01-14 14:22:59 -0300

    code_39_vin

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
 			target: document.querySelector('#contenedor'), // Donde se va a colocar el video de la "Cam"
 		},
 		decoder: {
-			readers: ["codabar_reader"]
+			readers: ["code_39_vin_reader"]
 		}
 	}, function (err) {
 		if (err) {

60b0bc955eb271ac8207493d9cd09eaf1adb4d4d
JoniMeichtry11 <joni.meichtry@gmail.com>
2022-01-14 14:40:32 -0300

    format scan full

--- index.js
+++ index.js
@@ -11,7 +11,17 @@ document.addEventListener("DOMContentLoaded", () => {
 			target: document.querySelector('#contenedor'), // Donde se va a colocar el video de la "Cam"
 		},
 		decoder: {
-			readers: ["2of5_reader"]
+			readers: [
+				"code_128_reader",
+				"ean_reader",
+				"ean_8_reader",
+				"code_39_reader",
+				"code_39_vin_reader",
+				"codabar_reader",
+				"upc_reader",
+				"upc_e_reader",
+				"i2of5_reader"
+			]
 		}
 	}, function (err) {
 		if (err) {
